d6f9ae98c7d1fedb218777f014d0fc3f2cb1bff2
Haaku <cccontactme1@gmail.com>
2024-10-12 07:20:37 +0100

    make website responsive

--- script.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.querySelector(".navbar").style.width = "250px";
-}
-
-function closeNav() {
-    document.querySelector(".navbar").style.width = "0";
-}
--- scripts.js
+++ scripts.js
@@ -23,3 +23,38 @@ Array.from(btns).forEach((btn) => {
         content.classList.remove("hidden")
     })
 })
+
+// Nav bar 
+function openNav() {
+    document.querySelector(".navbar").style.width = "250px";
+}
+
+function closeNav() {
+    document.querySelector(".navbar").style.width = "0";
+}
+
+
+
+
+
+
+
+// <!-- <script>
+// function showNav() {
+//     document.getElementById("navbar").style.display = "block"
+//     document.getElementById("navbar").style.transition = ".9s ease-in-out"
+
+// }
+// function closeNav() {
+//     document.getElementById("navbar").style.display = "none"
+// }
+
+// const navItems = document.querySelectorAll('.resume-items');
+// navItems.forEach(item => {
+//     item.addEventListener('click', () => {
+//         navItems.forEach(navItem => navItem.classList.remove('active'));
+//         this.classList.add('active');
+//     });
+// });
+
+// </script>
